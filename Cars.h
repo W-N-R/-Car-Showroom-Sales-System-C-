21c85c74ebc29fd0a342dc6c3d1d6ec34e3c3b2f
W-N-R <waleednaeem133@gmail.com>
2025-03-29 18:45:47 +0500

    .

--- Cars.h
+++ Cars.h
@@ -6,46 +6,41 @@
 #include <string>
 using namespace std;
 
-
-
-/////////////////////////////////CAR CLASS////////////////////////////////////////
 class Cars {
 protected:
-	string model;
-	string transmission;
-	string chassisNumber;
-	int manufactureYear;
-	double price;
-	double engineSize;
+    string model;
+    int year;
+    double price;
+    double engineCapacity;
+    string transmission;
+    string chassisNumber;
 public:
-	Cars() {}
-	Cars(string, int, double, double, string, string);
-	 void display();
-	 double getFinalPrice();
-	string getModel();
-
+    Cars(string model, int year, double price, double engineCapacity, string transmission, string chassisNumber);
+    virtual void display() const;
+    virtual double getFinalPrice() const;
+    virtual ~Cars() = default;
 };
 
-Cars::Cars(string model, int manufactureYear, double price, double engineSize, string transmission, string chassisNumber) {
-	this->model = model;
-	this->manufactureYear = manufactureYear;
-	this->price = price;
-	this->engineSize = engineSize;
-	this->transmission = transmission;
-	this->chassisNumber = chassisNumber;
-}
-void Cars::display() {
-	cout << " | Model: " << model << endl;;
-	cout << " | Year: " << manufactureYear << endl;
-	cout << " | Price: " << price << endl;
-	cout << " | Engine: " << engineSize << endl;
-	cout << " | Transmission: " << transmission << endl;
-}
-double Cars::getFinalPrice() {
-	return price;
-}
-string Cars::getModel() {
-	return model;
+Cars::Cars(string model, int year, double price, double engineCapacity, string transmission, string chassisNumber) {
+    this->model = model;
+    this->year = year;
+    this->price = price;
+    this->engineCapacity = engineCapacity;
+    this->transmission = transmission;
+    this->chassisNumber = chassisNumber;
 }
 
-#endif 
+void Cars::display() const {
+    cout << "Model: " << model << "\nYear: " << year << "\nPrice: $" << price << "\nEngine Capacity: "
+        << engineCapacity << "L\nTransmission: " << transmission << "\nChassis No: " << chassisNumber << "\n";
+}
+
+double Cars::getFinalPrice() const {
+    int currentYear = 2025; 
+    if (currentYear - year > 5) {
+        return price * 1.15; 
+    }
+    return price;
+}
+
+#endif // CARS_H
